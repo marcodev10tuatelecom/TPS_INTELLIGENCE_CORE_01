fa4adbc6bac5f86f4a83b8bf2e57edf47b825eda
marcodev10tuatelecom <marcodev10tuatelecom@gmail.com>
2026-09-01 18:27:20 -0300

    migration: make V0001 postcheck assertive

--- migrations/V0001/postcheck.sql
+++ migrations/V0001/postcheck.sql
@@ -1,7 +1,12 @@
--- V0001 POSTCHECK | R0
+-- V0001 POSTCHECK | READ ONLY | FAIL CLOSED
+WHENEVER SQLERROR EXIT SQL.SQLCODE
+SET SERVEROUTPUT ON
+
+@@../../tests/compile/COMP-000_kernel.sql
+
 SELECT object_type, status, COUNT(*) object_count
 FROM user_objects
-WHERE object_name LIKE 'TPS_%'
+WHERE object_name LIKE 'TPS\_%' ESCAPE '\'
 GROUP BY object_type,status
 ORDER BY object_type,status;
 
@@ -13,3 +18,5 @@ SELECT COUNT(*) relation_types FROM tps_relation_type;
 SELECT COUNT(*) context_types FROM tps_context_type;
 SELECT COUNT(*) event_types FROM tps_event_type;
 SELECT COUNT(*) vector_types FROM tps_vector_type;
+
+PROMPT V0001_POSTCHECK=PASS
